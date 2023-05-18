b95d26345ee77bce3a27d6fbff59847737b7105d
unknown <mitchellkirk2@yahoo.com>
2023-05-18 11:38:54 -0400

    added descriptive comments

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,15 +1,18 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+//Gets input from input field
 function getUserNumberInput(){
     return parseInt(userInput.value);
 }
 
+//Writes the calculation that is occuring to the log
 function createAndWriteOutput(operator, resultBeforeCalc, calcNumber){
     const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`
-    outputResult(currentResult, calcDescription);
+    outputResult(currentResult, calcDescription); //from vendor file
 }
 
+//adds the user input to the current result
 function add(){
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
@@ -17,6 +20,7 @@ function add(){
     createAndWriteOutput('+', initialResult, enteredNumber);
 }
 
+//subtracts the user input from the current result
 function subtract(){
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
@@ -24,6 +28,7 @@ function subtract(){
     createAndWriteOutput('-', initialResult, enteredNumber);
 }
 
+//multiplies the user input by the current result
 function multiply(){
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
@@ -31,6 +36,7 @@ function multiply(){
     createAndWriteOutput('*', initialResult, enteredNumber);
 }
 
+//divides the user input by the current result
 function divide(){
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
